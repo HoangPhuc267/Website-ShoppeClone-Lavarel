ad67534f249a5b2863475a51e6367060bc51c9cc
Hoang Phuc <phuccua7@gmail.com>
2026-04-28 16:09:16 +0700

    Fix lỗi HTTP và HTTPS trên Nginx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- 1. Thêm dòng này
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        // 2. Thêm đoạn code này vào đây
+        if (str_contains(request()->header('host'), 'ngrok-free.app')) {
+            URL::forceScheme('https');
+        }
     }
 }
